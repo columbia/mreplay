8ee8e26950b20ecc09037ccc90cb3ccc10f0c25f
Nicolas Viennot and Sid Nair <nviennot+sidnair@viennot.biz>
2011-11-22 00:11:52 -0500

    Added the record/replay scripts

--- mreplay/__init__.py
+++ mreplay/__init__.py
@@ -1,1 +1,1 @@
-from session import *
+from explorer import Explorer
---
+++ mreplay/explorer.py
@@ -0,0 +1,6 @@
+class Explorer:
+    def __init__(self, session):
+        self.session = session
+
+    def run(self):
+        pass
