720d66bda24d1710c619f9ca1d8600c02af55ab5
Nicolas Viennot and Sid Nair <nviennot+sidnair@viennot.biz>
2012-03-15 12:17:48 -0400

    Replace mutator description added

--- mreplay/mutator/replace.py
+++ mreplay/mutator/replace.py
@@ -4,6 +4,10 @@ class Replace(Mutator):
     def __init__(self, replacements):
         self.replacements = replacements
 
+    def __str__(self):
+        return "r-" + ",".join(map(lambda e: "%d:%d" % (e.proc.pid, e.index),
+                                   self.replacements.keys()))
+
     def process_events(self, events):
         for event in events:
             if self.replacements.has_key(event):
