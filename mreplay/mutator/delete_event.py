8b87dd53bf5e5d08bf05fc8b6080be8228aafe65
Nicolas Viennot and Sid Nair <nviennot+sidnair@viennot.biz>
2012-04-14 18:11:18 -0400

    Delete Mutator can handle multiple events

--- mreplay/mutator/delete_event.py
+++ mreplay/mutator/delete_event.py
@@ -4,45 +4,31 @@ from mreplay.location import Location
 from location_matcher import LocationMatcher
 
 class DeleteEvent(Mutator):
-    def __init__(self, event):
-        self.event = event
-        self.is_syscall = event.is_a(scribe.EventSyscallExtra)
-        self.matcher = LocationMatcher(Location(event, 'before'))
+    def __init__(self, events):
+        if not isinstance(events, list):
+            events = [events]
+        events = list(events)
+
+        self.events = list(events)
+        self.matcher = LocationMatcher(map(lambda e: Location(e, 'before'), events))
 
     def __str__(self):
-        return "d-%d:%d" % (self.event.proc.pid, self.event.index)
+        return "d-%d:%s" % (self.events[0].proc.pid,
+                ','.join(map(lambda e: str(e.index), self.events)))
 
     def process_events(self, events):
-        if self.event.is_a(scribe.EventSyscallExtra):
-            skip_events = False
-            for e in events:
-                match = self.matcher.match(e)
-                if match is not None:
-                    if not e.is_a(scribe.EventSyscallExtra):
-                        continue
-                    skip_events = True
-
-                if skip_events:
-                    if e.is_a(scribe.EventSyscallEnd):
-                        skip_events = False
-                else:
-                    yield e
-
-        elif self.event.is_a(scribe.EventResourceLockExtra):
-            skip_events = 0
-            for e in events:
-                match = self.matcher.match(e)
-                if match is not None or skip_events > 0:
-                    if e.is_a(scribe.EventResourceLockExtra):
-                        skip_events += 1
-                    elif e.is_a(scribe.EventResourceUnlock):
-                        skip_events -= 1
-                else:
-                    yield e
-
-        else:
-            for e in events:
-                match = self.matcher.match(e)
-                if match is not None:
-                    continue
+        syscall_depth = 0
+        res_depth = 0
+        for e in events:
+            match = self.matcher.match(e)
+            if match is not None or syscall_depth > 0 or res_depth > 0:
+                if e.is_a(scribe.EventSyscallExtra):
+                    syscall_depth += 1
+                elif e.is_a(scribe.EventResourceLock):
+                    res_depth += 1
+                elif e.is_a(scribe.EventSyscallEnd):
+                    syscall_depth -= 1
+                elif e.is_a(scribe.EventResourceUnlock):
+                    res_depth -= 1
+            else:
                 yield e
